37389add5597fab05a29211796490922b03443cd
Parimala797 <84122733+Parimala797@users.noreply.github.com>
2021-11-29 10:53:13 +0530

    Update Traffic_Light_Controller.v

--- Traffic_Light_Controller.v
+++ Traffic_Light_Controller.v
@@ -24,16 +24,16 @@ module Traffic_Light_Controller(
 
 
     input clk,rst,
-    output reg [2:0]light_M1,
-    output reg [2:0]light_S,
-    output reg [2:0]light_MT,
-    output reg [2:0]light_M2
+    output reg [2:0]M1,
+    output reg [2:0]SR,
+    output reg [2:0]MT,
+    output reg [2:0]M2
     );
     
     parameter  S1=0, S2=1, S3 =2, S4=3, S5=4,S6=5;
     reg [3:0]count;
     reg[2:0] ps;
-    parameter  sec7=7,sec5=5,sec2=2,sec3=3;
+    parameter  t1=7,t2=5,t3=2,t4=3;
 
    
     
@@ -50,7 +50,7 @@ module Traffic_Light_Controller(
        
             
             case(ps)
-                S1: if(count<sec7)
+                S1: if(count<t1)
                         begin
                         ps<=S1;
                         count<=count+1;
@@ -60,7 +60,7 @@ module Traffic_Light_Controller(
                         ps<=S2;
                         count<=0;
                         end
-                S2: if(count<sec2)
+                S2: if(count<t2)
                         begin
                         ps<=S2;
                         count<=count+1;
@@ -71,7 +71,7 @@ module Traffic_Light_Controller(
                         ps<=S3;
                         count<=0;
                         end
-                S3: if(count<sec5)
+                S3: if(count<t3)
                         begin
                         ps<=S3;
                         count<=count+1;
@@ -82,7 +82,7 @@ module Traffic_Light_Controller(
                         ps<=S4;
                         count<=0;
                         end
-                S4:if(count<sec2)
+                S4:if(count<t2)
                         begin
                         ps<=S4;
                         count<=count+1;
@@ -93,7 +93,7 @@ module Traffic_Light_Controller(
                         ps<=S5;
                         count<=0;
                         end
-                S5:if(count<sec3)
+                S5:if(count<t4)
                         begin
                         ps<=S5;
                         count<=count+1;
@@ -105,7 +105,7 @@ module Traffic_Light_Controller(
                         count<=0;
                         end
 
-                S6:if(count<sec2)
+                S6:if(count<t2)
                         begin
                         ps<=S6;
                         count<=count+1;
@@ -127,52 +127,52 @@ module Traffic_Light_Controller(
                      
                     S1:
                     begin
-                       light_M1<=3'b001;
-                       light_M2<=3'b001;
-                       light_MT<=3'b100;
-                       light_S<=3'b100;
+                       M1<=3'b001;
+                       M2<=3'b001;
+                       MT<=3'b100;
+                       SR<=3'b100;
                     end
                     S2:
                     begin 
-                       light_M1<=3'b001;
-                       light_M2<=3'b010;
-                       light_MT<=3'b100;
-                       light_S<=3'b100;
+                       M1<=3'b001;
+                       M2<=3'b010;
+                       MT<=3'b100;
+                       S<=3'b100;
                     end
                     S3:
                     begin
-                       light_M1<=3'b001;
-                       light_M2<=3'b100;
-                       light_MT<=3'b001;
-                       light_S<=3'b100;
+                       M1<=3'b001;
+                       M2<=3'b100;
+                       MT<=3'b001;
+                       SR<=3'b100;
                     end
                     S4:
                     begin
-                       light_M1<=3'b010;
-                       light_M2<=3'b100;
-                       light_MT<=3'b010;
-                       light_S<=3'b100;
+                       M1<=3'b010;
+                       M2<=3'b100;
+                       MT<=3'b010;
+                       SR<=3'b100;
                     end
                     S5:
                     begin
-                       light_M1<=3'b100;
-                       light_M2<=3'b100;
-                       light_MT<=3'b100;
-                       light_S<=3'b001;
+                       M1<=3'b100;
+                       M2<=3'b100;
+                       MT<=3'b100;
+                       SR<=3'b001;
                     end
                     S6:
                     begin 
-                       light_M1<=3'b100;
-                       light_M2<=3'b100;
-                       light_MT<=3'b100;
-                       light_S<=3'b010;
+                       M1<=3'b100;
+                       M2<=3'b100;
+                       MT<=3'b100;
+                       SR<=3'b010;
                     end
                     default:
                     begin 
-                       light_M1<=3'b000;
-                       light_M2<=3'b000;
-                       light_MT<=3'b000;
-                       light_S<=3'b000;
+                       M1<=3'b000;
+                       M2<=3'b000;
+                       MT<=3'b000;
+                       SR<=3'b000;
                     end
                     endcase
             end                
@@ -182,11 +182,11 @@ endmodule
 
 module Traffic_Light_Controller_tb;
 reg clk,rst;
-wire [2:0]light_M1;
-wire [2:0]light_S;
-wire [2:0]light_MT;
-wire [2:0]light_M2;
-Traffic_Light_Controller dut(.clk(clk), .rst(rst), .light_M1(light_M1), .light_S(light_S), .light_MT(light_MT), .light_M2(light_M2));
+wire [2:0]M1;
+wire [2:0]SR;
+wire [2:0]MT;
+wire [2:0]M2;
+Traffic_Light_Controller dut(.clk(clk), .rst(rst), .M1(M1), .SR(SR), .MT(MT), .M2(M2));
 initial 
     begin
         $dumpfile("Traffic_Light_Controller.vcd");
